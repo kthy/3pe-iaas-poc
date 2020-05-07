9daef17eaa5257e8279940debaad1b587c2086d4
Kristian Thy <thy@42.dk>
2020-05-08 00:58:16 +0200

    fix: move configure list to 99-status.sh

--- provisioning/02-aws-configure.sh
+++ provisioning/02-aws-configure.sh
@@ -5,4 +5,3 @@ aws configure set default.aws_access_key_id $1
 aws configure set default.aws_secret_access_key $2
 aws configure set default.region $3
 aws configure set default.output json
-aws configure list
